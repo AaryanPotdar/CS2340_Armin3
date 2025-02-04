d2e2c671b0c300fd713bf5583bb231127c7feb38
lilychisholm <lsc112003@gmail.com>
2025-02-04 16:31:54 -0500

    SQLite to MySQL

--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -79,19 +79,19 @@ WSGI_APPLICATION = 'moviesstore.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    #  for MySQL
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'moviesstore',
-    #     'USER': 'root',
-    #     'PASSWORD': '',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    #  for MySQL
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'moviesstore',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
 }
 
 
