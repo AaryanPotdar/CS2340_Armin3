e11b75925d141c491c03c50fb425b48f25ebabd5
Spl112 <simplistic6231@gmail.com>
2025-02-10 19:20:28 -0500

    Tried to add password reset, due to previous errors regarding mysql I am unable to run server to test password reset functionality, thus testing may be needed

--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -143,3 +143,12 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# email configs
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+# email above ^^^^ I believe is the email address that interacts with the user email for password resets
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
+EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
